d0f4bbff55562baa425e8500b447d25c700d4174
anil kumar <anil.motupalli@live.com>
2017-04-12 17:22:08 -0700

    worked on county geo map

--- Sample_Data/FIPS_seperate_mapper.py
+++ Sample_Data/FIPS_seperate_mapper.py
@@ -1,6 +1,21 @@
 import pandas as pd
+import json
 
-fips = pd.read_csv('FIPS_seperate.csv', dtype={'FIPS_State': object,'FIPS_County': object})
-fips = fips.drop(['H'], axis=1)
-fips['County_State'] = fips['County'] + ', ' + fips['State']
-fips.to_csv('FIPS_Cleaned.csv')
+# fips = pd.read_csv('FIPS_seperate.csv', dtype={'FIPS_State': object,'FIPS_County': object})
+# fips = fips.drop(['H'], axis=1)
+# fips['County_State'] = fips['County'] + ', ' + fips['State']
+# fips.to_csv('FIPS_Cleaned.csv')
+
+state_counties = {}
+fips = pd.read_csv('FIPS_mapping.csv', dtype={'FIPS': object})
+states = [stat[1:] for stat in pd.unique(fips['State'])]
+
+fips['FIPS'] = "0500000US" + fips['FIPS']
+# print(states)
+for state in states:
+    state_counties[state] = fips['FIPS'][fips['State']
+                                         == ' ' + state].values.tolist()
+with open('state_counties.json', mode='w') as scjs:
+    json.dump(state_counties, scjs)
+# print(state_counties)
+# print(fips.groupby(by='State'))
